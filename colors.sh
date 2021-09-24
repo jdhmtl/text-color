f5c7c6173ce6824edd8894f1c7c1a10e67b2566a
John Hodges <jdhmtl@users.noreply.github.com>
2021-09-24 16:59:26 -0400

    Remove redundant code

--- colors.sh
+++ colors.sh
@@ -2,7 +2,7 @@
 
 TEXT="Sample Text"
 NORMAL='m 30m 31m 32m 33m 34m 35m 36m 37m'
-BRIGHT='1m 1;30m 1;31m 1;32m 1;33m 1;34m 1;35m 1;36m 1;37m';
+BRIGHT='1m 1;30m 1;31m 1;32m 1;33m 1;34m 1;35m 1;36m 1;37m'
 
 print_colors() {
     read -ra codes <<< $1
